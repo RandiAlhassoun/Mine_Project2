225460f1fd687deba3b2e0cd103eecfff82a4105
RAND ALHASSOUN <rialhassoun22@twq.idserve.net>
2022-12-07 09:30:23 +0300

    Initial Commit

---
+++ Mine_Project2/Mine_Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mine_Project2App.swift
+//  Mine_Project2
+//
+//  Created by Rand Alhassoun on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Mine_Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
