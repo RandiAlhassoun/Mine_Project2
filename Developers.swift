d5bc001dedfff64bb5cbaa2c28f14d8e671f12a3
Shifa <shifaabdulaziz@hotmail.com>
2022-12-08 13:03:40 +0300

    .

---
+++ Developers.swift
@@ -0,0 +1,58 @@
+//
+//  Developers.swift
+//  Mine_Project2
+//
+//  Created by Shifa Alfaisal on 14/05/1444 AH.
+//
+
+//
+//  ContentView.swift
+//  Programming
+//
+//  Created by Shifa Alfaisal on 12/05/1444 AH.
+//
+
+import SwiftUI
+struct User: Identifiable {
+var id: Int
+let title, value, imageName: String
+}
+
+
+struct DeveloperScreen: View {
+    var users: [User] = [
+        .init(id: 0, title: "Abdallah Mohamed" , value: "IOS Develorer" , imageName: "A"),
+        .init(id: 1, title: "Faisal AlThaqafi" , value: "Java Developer" , imageName: "B"),
+        .init(id: 2, title: "Waleed" , value: "Python - Java Developer" , imageName: "A"),
+        .init(id: 3, title: "Sara" , value: "IOS Developer" , imageName: "B"),
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                
+                ForEach(users, id: \.id) { user in
+                    HStack {
+                        Image(user.imageName)
+                    .resizable()
+                    .frame(width: 50, height: 50, alignment: .center)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.black, lineWidth: 2.0))
+                VStack(alignment: .leading) {
+                    Text(user.title).font(.headline)
+                    Text(user.value).font(.subheadline)
+                }
+                    }.padding(.init(top: 12, leading: 0, bottom: 12, trailing: 0))
+                }
+            }
+            }
+        }
+    }
+
+        struct DeveloperScreen_Previews: PreviewProvider {
+            static var previews: some View {
+                DeveloperScreen()
+            }
+        }
+    
+
--- Home Page1.swift
+++ Home Page1.swift
@@ -1,4 +1,4 @@
-//
+
 //  Home Page1.swift
 //  Mine_Project2
 //
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-struct HomePage1: View {
+struct DeveloperView: View {
     @State private var searchText = ""
     @State private var isAllingClicke = false
     @State private var isIosClicked = false
@@ -17,61 +17,65 @@ struct HomePage1: View {
     var body: some View {
         if #available(iOS 16.0, *) {
             NavigationStack {
-            ZStack{
-
-                HStack {
-                    VStack(alignment: .leading, spacing: 8.0){
-                        Text("Developers In")
-                        HStack(spacing: 15.0){
-                            Button("All"){
-                                
-                                isAllingClicke.toggle()
-                            }
-                            .buttonStyle(.bordered)
-                            .foregroundColor(Color(.black))
-                            .background(isAllingClicke ? Color(red: 0.176, green: 0.217, blue: 0.479) : Color(UIColor.systemBackground) )
-                            .cornerRadius(5)
-                            
-                            Button("Java"){
-                                
-                                isJavaClicked.toggle()
-                            }
-                            .buttonStyle(.bordered)
-                            .foregroundColor(Color(.black))
-                            .background(isJavaClicked ? Color(red: 0.176, green: 0.217, blue: 0.479) : Color(UIColor.systemBackground) )
-                            .cornerRadius(5)
-                            
-                            Button("IOS"){
-                                
-                                isIosClicked.toggle()
-                            }
-                            .buttonStyle(.bordered)
-                            .foregroundColor(Color(.black))
-                            .background(isIosClicked ? Color(red: 0.176, green: 0.217, blue: 0.479) : Color(UIColor.systemBackground) )
-                            .cornerRadius(5)
-                            Button("#C"){
-                                
-                                iscClicked.toggle()
-                            }
-                            .buttonStyle(.bordered)
-                            .foregroundColor(Color(.black))
-                            .background(iscClicked ? Color(red: 0.176, green: 0.217, blue: 0.479) : Color(UIColor.systemBackground) )
-                            .cornerRadius(5)
-                        }
-                    }
-                    .padding(.bottom, 550.0)
+                ZStack{
+                  
                     
-                    Spacer()
+                    HStack {
+                        VStack(alignment: .leading, spacing: 8.0){
+                            Text("Developers In")
+                            HStack(spacing: 15.0){
+                                Button("All"){
+                                    
+                                    isAllingClicke.toggle()
+                                }
+                                .buttonStyle(.bordered)
+                                .foregroundColor(Color(.black))
+                                .background(isAllingClicke ? Color(red: 0.176, green: 0.217, blue: 0.479) : Color(UIColor.systemBackground) )
+                                .cornerRadius(5)
+                                
+                                Button("Java"){
+                                    
+                                    isJavaClicked.toggle()
+                                }
+                                .buttonStyle(.bordered)
+                                .foregroundColor(Color(.black))
+                                .background(isJavaClicked ? Color(red: 0.176, green: 0.217, blue: 0.479) : Color(UIColor.systemBackground) )
+                                .cornerRadius(5)
+                                
+                                Button("IOS"){
+                                    
+                                    isIosClicked.toggle()
+                                }
+                                .buttonStyle(.bordered)
+                                .foregroundColor(Color(.black))
+                                .background(isIosClicked ? Color(red: 0.176, green: 0.217, blue: 0.479) : Color(UIColor.systemBackground) )
+                                .cornerRadius(5)
+                                Button("#C"){
+                                    
+                                    iscClicked.toggle()
+                                }
+                                .buttonStyle(.bordered)
+                                .foregroundColor(Color(.black))
+                                .background(iscClicked ? Color(red: 0.176, green: 0.217, blue: 0.479) : Color(UIColor.systemBackground) )
+                                .cornerRadius(5)
+                            }
+                        }
+                        .padding(.bottom, 550.0)
+                        
+                        Spacer()
+                    }
+                    .padding()
+                    
+                    DeveloperScreen()
+                        .padding(.top,100)
                 }
-                .padding()
-            }
                 
                 
                 .navigationTitle("Explore")
-
+                
                 .navigationBarColor(backgroundColor: UIColor(red: 0.18, green: 0.22, blue: 0.48, alpha: 1.00)
-, tintColor: .white)
-
+                                    , tintColor: .white)
+                
             }
             
             .searchable(text: $searchText)
@@ -80,14 +84,10 @@ struct HomePage1: View {
             
         }
     }
-    
-    
-    struct HomePage1_Previews: PreviewProvider {
-        static var previews: some View {
-            HomePage1()
-        }
-    }
 }
+    
+
+
 
 
 extension View {
@@ -95,3 +95,9 @@ extension View {
     self.modifier(NavigationBarColor(backgroundColor: backgroundColor, tintColor: tintColor))
   }
 }
+struct HomePage1_Previews: PreviewProvider {
+    static var previews: some View {
+        DeveloperView()
+    }
+}
+
