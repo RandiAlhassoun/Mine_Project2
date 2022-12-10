2dc15c415df73a293f3485f612e53543669ca3ac
Shifa <shifaabdulaziz@hotmail.com>
2022-12-10 15:01:34 +0300

    .

---
+++ login .swift
@@ -0,0 +1,20 @@
+//
+//  login .swift
+//  Mine_Project2
+//
+//  Created by Shifa Alfaisal on 16/05/1444 AH.
+//
+
+import SwiftUI
+
+struct login_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct login__Previews: PreviewProvider {
+    static var previews: some View {
+        login_()
+    }
+}
