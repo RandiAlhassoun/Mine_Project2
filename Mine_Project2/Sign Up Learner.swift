4dbb031503c86f59addc9a6f0d7c5de7620cc3ef
RAND ALHASSOUN <rialhassoun22@twq.idserve.net>
2022-12-14 15:23:14 +0300

    test

--- Mine_Project2/Sign Up Learner.swift
+++ Mine_Project2/Sign Up Learner.swift
@@ -6,8 +6,22 @@
 //
 
 import SwiftUI
+import CloudKit
+
 
 struct Sign_Up_Learner: View {
+//    @State private var password = ""
+//    @State private var wrongPassword: Float  = 0
+//    @State private var passwordConfirm = ""
+//    @State private var wrongPasswordConfirm: Float  = 0
+    
+    @State  var Name = ""
+    @State  var Email = ""
+    @State private var password = ""
+    @State private var passwordConfirm = ""
+
+    @State var toContentView = false
+    
     var body: some View {
         VStack{
             //=========================================
@@ -30,7 +44,8 @@ struct Sign_Up_Learner: View {
                 Text("Inter your Name")
                    // .padding()
                     //.multilineTextAlignment(.leading)
-                TextField("Write your name", text: .constant(""))
+               // TextField("Write your name", text: .constant(""))
+                TextField("Name", text: $Name)
                     .frame(width: 350, height: 40)
                     .foregroundColor(.black)
                     .background(Color(red: 0.933, green: 0.933, blue: 0.938))
@@ -41,61 +56,141 @@ struct Sign_Up_Learner: View {
                 Text("Inter your Email")
                    // .padding()
                     //.multilineTextAlignment(.leading)
-                TextField("Write your Email", text: .constant(""))
+                TextField("Write your Email", text:$Email)
                     .frame(width: 350, height: 40)
                     .foregroundColor(.black)
                     .background(Color(red: 0.933, green: 0.933, blue: 0.938))
                     .cornerRadius(5)
-                  //.padding()
-                //-----------Password--------------------------------
+                  //.padding()  //-----------Password--------------------------------
                 Text("Inter your Password")
                    // .padding()
                     //.multilineTextAlignment(.leading)
-                TextField("Write your Password", text: .constant(""))
+                
+                SecureField("Inter your Password", text: $password)
+                    .padding()
                     .frame(width: 350, height: 40)
                     .foregroundColor(.black)
                     .background(Color(red: 0.933, green: 0.933, blue: 0.938))
                     .cornerRadius(5)
-                    //.padding()
+                
+                
                 //-----------Confirm Password-------------------------
                 Text("Inter Confirm Password")
-                   // .padding()
-                    //.multilineTextAlignment(.leading)
-                TextField("Confirm Password", text: .constant(""))
-                    .frame(width: 350, height: 40)
-                    .foregroundColor(.black)
-                    .background(Color(red: 0.933, green: 0.933, blue: 0.938))
-                    .cornerRadius(5)
-                    //.padding()
+            
+            SecureField("Inter Confirm Password", text: $passwordConfirm)
+                .padding()
+                .frame(width: 350, height: 40)
+                .foregroundColor(.black)
+                .background(Color(red: 0.933, green: 0.933, blue: 0.938))
+                .cornerRadius(5)
+//
+                //====================================================
                 
+                VStack(alignment: .leading){
+        
+                }//info vstack
+//                .padding()
+////                Spacer()
+//                Button {
+////                    AddLearner(Name : Name , Email : Email ,password : password, passwordConfirm : passwordConfirm)
+//                } label: {
+//                    Text("Next").font(.title3).padding()
+//                }
+//======================
                 
             }//info vstack
             .padding()
             Spacer()
-            NavigationLink(destination: ContentView()) {
-               Text("Sign up").font(.title3).padding()
-                //.frame()
+            Button {
+                AddLearner(Name : Name , Email : Email ,password : password, passwordConfirm : passwordConfirm)
+            } label: {
+                Text("Next").font(.title3).padding()
                     .frame(width: 350, height: 40)
                     .foregroundColor(.white)
                     .background(Color(red: 0.176, green: 0.217, blue: 0.479))
                     .cornerRadius(5)
                     .padding()
             }
-            HStack{
-            Text("You  have an account?")
-                .foregroundColor(.gray)
+
+//            HStack{
+//            Text("You  have an account?")
+//                .foregroundColor(.gray)
+//
+//            NavigationLink(destination: ContentView()) {
+//               Text("Login")
+//                    .foregroundColor(Color(red: 0.176, green: 0.217, blue: 0.479))
+//            }
+//        }//hstack
+//
+
             
-            NavigationLink(destination: ContentView()) {
-               Text("Login")
-                    .foregroundColor(Color(red: 0.176, green: 0.217, blue: 0.479))
-            }
-        }//hstack
+            //            NavigationLink(destination: ContentView()) {
+//               Text("Sign up").font(.title3).padding()
+//                //.frame()
+//                    .frame(width: 350, height: 40)
+//                    .foregroundColor(.white)
+//                    .background(Color(red: 0.176, green: 0.217, blue: 0.479))
+//                    .cornerRadius(5)
+//                    .padding()
+//            }
+//            HStack{
+//            Text("You  have an account?")
+//                .foregroundColor(.gray)
+//
+//            NavigationLink(destination: ContentView()) {
+//               Text("Login")
+//                    .foregroundColor(Color(red: 0.176, green: 0.217, blue: 0.479))
+//            }
+//        }//hstack
                 
         }//vs
+        .fullScreenCover(isPresented: $toContentView) {
+            ContentView()
+        }
+        
         .navigationTitle("Sign UP")
         .navigationBarColor(backgroundColor: UIColor(red: 0.18, green: 0.22, blue: 0.48, alpha: 1.00)
         , tintColor: .white)
+        
+        //========================================
+//        func AddLearner(Name : String, Email : String , password : String , passwordConfirm : StringSign_Up_Learner){
+//            let record = CKRecord(recordType: "Profile")
+//            record["Name"] = Name
+//            record["lastName"] = Email
+//            record["major"] = password
+//            record["age"] = passwordConfirm
+//
+//            CKContainer.init(identifier: "com.exame.Mine-Project2").publicCloudDatabase.save(record){
+//                record, error in
+//                guard error == nil else{
+//                    print(error?.localizedDescription)
+//                    return
+//                }
+//                Thread.sleep(forTimeInterval: 0.7)
+//                toContentView.toggle()
+//
+//            }
+//        }
+
     }//body
+    func AddLearner(Name : String, Email : String , password : String , passwordConfirm : String){
+        let record = CKRecord(recordType: "Learner")
+        record["UserName"] = Name
+        record["Email"] = Email
+        record["password"] = password
+        record["passwordConfirm"] = passwordConfirm
+                                   
+        CKContainer.init(identifier: "iCloud.com.exame.Mine-Project2").publicCloudDatabase.save(record){
+            record, error in
+            guard error == nil else{
+                print(error?.localizedDescription)
+                return
+            }
+            Thread.sleep(forTimeInterval: 0.7)
+            toContentView.toggle()
+
+        }
+    }
 }
 
 struct Sign_Up_Learner_Previews: PreviewProvider {
